2eefd61f5e38ded6d727c8e147eb7d619bf6c85e
Daleth Darko <daleth@Dalethium.com>
2018-10-23 12:40:57 +0200

    Added AbstractScriptManager

---
+++ src/main/java/coffee/weneed/utils/scripting/AbstractScriptManager.java
@@ -0,0 +1,46 @@
+package coffee.weneed.utils.scripting;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public abstract class AbstractScriptManager {
+
+	private static final ScriptEngineManager sem = new ScriptEngineManager();
+
+	protected Invocable getInvocable(Path path) {
+		ScriptEngine se = getScriptEngine(path);
+		return se == null ? null : (Invocable) se;
+	}
+
+	protected ScriptEngine getScriptEngine(Path path) {
+		FileReader fr = null;
+		try {
+			File script = path.toFile();
+			if (!script.exists()) {
+				return null;
+			}
+			ScriptEngine engine = AbstractScriptManager.sem.getEngineByName("nashorn");
+			fr = new FileReader(script);
+			engine.eval("load('nashorn:mozilla_compat.js')");
+			engine.eval(fr);
+			return engine;
+		} catch (ScriptException | FileNotFoundException e) {
+			return null;
+		} finally {
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+			}
+		}
+	}
+}
