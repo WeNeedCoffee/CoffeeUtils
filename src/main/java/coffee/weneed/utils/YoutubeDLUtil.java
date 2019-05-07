72cae45e9789278b5516a2b60f51b945e8f7768f
Daleth Darko <Daleth@Dalethium.com>
2019-05-07 16:04:23 -0700

    removed unused imports

--- src/main/java/coffee/weneed/utils/YoutubeDLUtil.java
+++ src/main/java/coffee/weneed/utils/YoutubeDLUtil.java
@@ -1,13 +1,8 @@
 package coffee.weneed.utils;
 
-import java.io.File;
-import com.sapher.youtubedl.YoutubeDL;
-import com.sapher.youtubedl.YoutubeDLException;
-import com.sapher.youtubedl.YoutubeDLRequest;
-
 public class YoutubeDLUtil {
 
-	public static File downloadMP3(String exec, String video, String path, String filename) {
+	/*public static File downloadMP3(String exec, String video, String path, String filename) {
 		if (!path.endsWith(File.separator)) {
 			path = path + File.separator;
 		}
@@ -22,5 +17,5 @@ public class YoutubeDLUtil {
 			e.printStackTrace();
 		}
 		return new File(path + filename + ".mp3");
-	}
+	}*/
 }
