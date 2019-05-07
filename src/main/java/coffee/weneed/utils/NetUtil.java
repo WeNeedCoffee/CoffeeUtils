b2ee208f4688fdac8a4f8076b9a4bcbeffc5e2c0
Daleth Darko <Daleth@Dalethium.com>
2019-05-06 20:34:02 -0700

    Added email validation to NetUtil

--- src/main/java/coffee/weneed/utils/NetUtil.java
+++ src/main/java/coffee/weneed/utils/NetUtil.java
@@ -1,12 +1,25 @@
 package coffee.weneed.utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,6 +54,42 @@ public class NetUtil {
 	}
 
 	/**
+	 * https://www.dev2qa.com/check-valid-www-domain-and-hostname-java-examples/
+	 * addStr : A domain string. return : true means addStr is a valid domain name,
+	 * false means addStr is not a valide domain name.
+	 */
+	public static boolean isValidDomain(String addStr) {
+		boolean ret = true;
+		if ("".equals(addStr) || addStr == null) {
+			ret = false;
+		} else if (addStr.startsWith("-") || addStr.endsWith("-")) {
+			ret = false;
+		} else if (addStr.indexOf(".") == -1) {
+			ret = false;
+		} else {
+			String domainEle[] = addStr.split("\\.");
+			int size = domainEle.length;
+			for (int i = 0; i < size; i++) {
+				String domainEleStr = domainEle[i];
+				if ("".equals(domainEleStr.trim())) {
+					return false;
+				}
+			}
+			char[] domainChar = addStr.toCharArray();
+			size = domainChar.length;
+			for (int i = 0; i < size; i++) {
+				char eleChar = domainChar[i];
+				String charStr = String.valueOf(eleChar);
+				if (!".".equals(charStr) && !"-".equals(charStr) && !charStr.matches("[a-zA-Z]") && !charStr.matches("[0-9]")) {
+					ret = false;
+					break;
+				}
+			}
+		}
+		return ret;
+	}
+
+	/**
 	 * https://stackoverflow.com/questions/2295221/java-net-url-read-stream-to-byte
 	 *
 	 * @author StackOverflow:ron-reiter
@@ -64,6 +113,149 @@ public class NetUtil {
 		}
 		stream.close();
 		return outputStream.toByteArray();
+	}
+
+	/***
+	 * https://www.rgagnon.com/javadetails/java-0452.html
+	 * @param hostName
+	 * @return
+	 * @throws NamingException
+	 */
+	public static boolean hasMX(String hostName) throws NamingException {
+		return getMX(hostName) != null && getMX(hostName).size() > 0;
+	}
+
+	private static int hear(BufferedReader in) throws IOException {
+		String line = null;
+		int res = 0;
+		while ((line = in.readLine()) != null) {
+			String pfx = line.substring(0, 3);
+			try {
+				res = Integer.parseInt(pfx);
+			} catch (Exception ex) {
+				res = -1;
+			}
+			if (line.charAt(3) != '-')
+				break;
+		}
+		return res;
+	}
+
+	private static void say(BufferedWriter wr, String text) throws IOException {
+		wr.write(text + "\r\n");
+		wr.flush();
+		return;
+	}
+
+	/***
+	 * https://www.rgagnon.com/javadetails/java-0452.html
+	 * @param hostName
+	 * @return
+	 * @throws NamingException
+	 */
+	public static ArrayList<String> getMX(String hostName) throws NamingException {
+		Hashtable<String, String> env = new Hashtable<>();
+		env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+		DirContext ictx = new InitialDirContext(env);
+		Attributes attrs = ictx.getAttributes(hostName, new String[] { "MX" });
+		Attribute attr = attrs.get("MX");
+		if ((attr == null) || (attr.size() == 0)) {
+			attrs = ictx.getAttributes(hostName, new String[] { "A" });
+			attr = attrs.get("A");
+			if (attr == null)
+				throw new NamingException("No match for name '" + hostName + "'");
+		}
+		ArrayList<String> res = new ArrayList<String>();
+		NamingEnumeration<?> en = attr.getAll();
+
+		while (en.hasMore()) {
+			String mailhost;
+			String x = (String) en.next();
+			String f[] = x.split(" ");
+			if (f.length == 1)
+				mailhost = f[0];
+			else if (f[1].endsWith("."))
+				mailhost = f[1].substring(0, (f[1].length() - 1));
+			else
+				mailhost = f[1];
+			res.add(mailhost);
+		}
+		return res;
+	}
+
+	/***
+	 * https://www.rgagnon.com/javadetails/java-0452.html
+	 * @param address
+	 * @return
+	 */
+	public static boolean isEmailValid(String address) {
+		int pos = address.indexOf('@');
+		if (pos == -1)
+			return false;
+		String domain = address.substring(++pos);
+		ArrayList<String> mxList = null;
+		try {
+			mxList = getMX(domain);
+		} catch (NamingException ex) {
+			return false;
+		}
+		if (mxList.size() == 0)
+			return false;
+		for (int mx = 0; mx < mxList.size(); mx++) {
+			boolean valid = false;
+			try {
+				int res;
+				Socket skt = new Socket((String) mxList.get(mx), 25);
+				BufferedReader rdr = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+				BufferedWriter wtr = new BufferedWriter(new OutputStreamWriter(skt.getOutputStream()));
+				res = hear(rdr);
+				if (res != 220) {
+					rdr.close();
+					wtr.close();
+					skt.close();
+					throw new Exception("Invalid header");
+				}
+				say(wtr, "EHLO lilith.cloud");
+
+				res = hear(rdr);
+				if (res != 250) {
+					rdr.close();
+					wtr.close();
+					skt.close();
+					throw new Exception("Not ESMTP");
+				}
+				say(wtr, "MAIL FROM: <spam@lilith.cloud>");
+				res = hear(rdr);
+				if (res != 250) {
+					rdr.close();
+					wtr.close();
+					skt.close();
+					throw new Exception("Sender rejected");
+				}
+				say(wtr, "RCPT TO: <" + address + ">");
+				res = hear(rdr);
+				say(wtr, "RSET");
+				hear(rdr);
+				say(wtr, "QUIT");
+				hear(rdr);
+				if (res != 250) {
+					rdr.close();
+					wtr.close();
+					skt.close();
+					throw new Exception("Address is not valid!");
+				}
+				valid = true;
+				rdr.close();
+				wtr.close();
+				skt.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			} finally {
+				if (valid)
+					return true;
+			}
+		}
+		return false;
 	}
 
 	/***
