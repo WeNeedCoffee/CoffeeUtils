fb4828016585786f8ecbfc54bb0fed947aa1766f
Daleth Darko <Daleth@Dalethium.com>
2019-12-11 01:26:08 -0800

    Showcase broken code for later fixing

--- src/test/java/coffee/weneed/utils/tests/CoffeeTests.java
+++ src/test/java/coffee/weneed/utils/tests/CoffeeTests.java
@@ -10,9 +10,15 @@ public class CoffeeTests {
 	@Test
 	public void testSN() throws UnknownHostException, IOException {
 		CoffeeWriter cw = new CoffeeWriter();
-		System.out.println(Float.MAX_VALUE - 2);
-		cw.writeSmart(Float.MAX_VALUE - 2);
+		System.out.println(2222222222222222222222.22d);
+		cw.writeSmart(2222222222222222222222.22d);
 		System.out.println(cw.getSize());
-		System.out.println(new CoffeeReader(cw.getByteArray()).readSmart().floatValue());
+		System.out.println(new CoffeeReader(cw.getByteArray()).readSmart().doubleValue());
+		/*
+		 * TODO: This outputs wrong
+		 * 2.2222222222222222E21
+		 * 5
+		 * 2.222222282395004E21
+		 */
 	}
 }
